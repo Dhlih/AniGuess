67ad1754c2a86010b2652f4933d071dd9175f93c
ifad <yusuffadhlih123@gmail.com>
2026-01-21 19:55:47 +0700

    added endpoint for get room, get room status, and join room

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const roomController = require("../controllers/roomController");
 
 router.get("/", roomController.getRooms);
+
+router.get("/:roomId", roomController.getRoom);
+
+router.get("/:roomId/status", roomController.getRoomStatus);
+
 router.post("/", roomController.createRoom);
+
+router.post("/join", roomController.joinRoom);
 
 module.exports = router;
