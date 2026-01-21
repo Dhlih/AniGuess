d28772b2132fa100113bbeda8a16694dc1f98db2
ifad <yusuffadhlih123@gmail.com>
2026-01-21 19:55:11 +0700

    added has credentials middleware

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ListRooms from "./pages/ListRooms";
-import Playing from "./pages/Playing";
+import RoomPage from "./pages/RoomPage";
 
 const App = () => {
+  const username = localStorage.getItem("aniguess_username");
+  const userId = localStorage.getItem("aniguess_uid");
+  const hasCredentials = username && userId ? true : false;
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<ListRooms />} />
-        <Route path="/play/room/:roomId" element={<Playing />} />
-        <Route path="/play/solo" element={<Playing />} />
+
+        <Route
+          path="/rooms"
+          element={hasCredentials ? <ListRooms /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/rooms/:id"
+          element={hasCredentials ? <RoomPage /> : <Navigate to="/" />}
+        />
+
+        {/* Redirect jika user mengetik URL ngawur */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
