74a6a5b45bd7d677e9f55c8c680fc15650f094c2
ifad <yusuffadhlih123@gmail.com>
2026-01-23 15:37:41 +0700

    deleted join room and get foom function

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -20,42 +20,7 @@ const getRooms = async (req, res) => {
         };
       }),
     );
-    res.json({ success: true, data: rooms });
-  } catch (error) {
-    res.status(500).json({ success: false, data: null });
-  }
-};
-
-const getRoom = async (req, res) => {
-  const { roomId } = req.params;
-
-  try {
-    let players = await client.ZRANGE(`rooms:${roomId}:scores`, 0, -1);
-    const formattedPlayers = players.map((player) => {
-      const [uid, username] = player.split(":");
-      return {
-        id: uid,
-        username,
-      };
-    });
-
-    const totalPlayers = players.length;
-
-    const room = await client.HGETALL(
-      `rooms:${roomId}:details`,
-      "max_players",
-      "host_id",
-    );
-
-    res.json({
-      success: true,
-      data: {
-        players: formattedPlayers,
-        max_players: room.max_players,
-        host_id: room.host_id,
-        total_players: totalPlayers,
-      },
-    });
+    res.status(200).json({ success: true, data: rooms });
   } catch (error) {
     res.status(500).json({ success: false, data: null });
   }
@@ -88,7 +53,7 @@ const createRoom = async (req, res) => {
       },
     ]);
 
-    res.json({
+    res.status(201).json({
       success: true,
       roomId: roomId,
       details: req.body,
@@ -103,24 +68,10 @@ const getRoomStatus = async (req, res) => {
 
   try {
     const roomStatus = await client.HGET(`rooms:${roomId}:details`, "status");
-    res.json({ success: true, data: { room_status: roomStatus } });
+    res.status(200).json({ success: true, data: { room_status: roomStatus } });
   } catch (error) {
     res.status(500).json({ success: false, data: null });
   }
 };
 
-const joinRoom = async (req, res) => {
-  const { player_id, player_username, room_id } = req.body;
-
-  try {
-    await client.ZADD(`rooms:${room_id}:scores`, {
-      score: 0,
-      value: `${player_id}:${player_username}`,
-    });
-    res.json({ success: true, data: null });
-  } catch (error) {
-    res.status(500).json({ success: false, data: null });
-  }
-};
-
-module.exports = { getRooms, createRoom, getRoomStatus, joinRoom, getRoom };
+module.exports = { getRooms, createRoom, getRoomStatus, getRoom };
