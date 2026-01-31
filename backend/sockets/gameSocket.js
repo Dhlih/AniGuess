6a6af99861fa15ba56bf2cd73e22497a04147a7d
ifad <yusuffadhlih123@gmail.com>
2026-02-01 00:21:37 +0700

    added submit answer event and set initial song when player started the game

--- backend/sockets/gameSocket.js
+++ backend/sockets/gameSocket.js
@@ -1,17 +1,54 @@
 const { client } = require("../config/redis");
+const getRandomSong = require("../helper/getRandomSong");
 
 module.exports = (io) => {
   io.on("connection", (socket) => {
     socket.on("start-game", async ({ room_id }) => {
-      try {
-        await client.HSET(`rooms:${room_id}:details`, "status", "playing");
-        io.to(room_id).emit("game-started", {
-          status: "playing",
-        });
-        console.log("mantap");
-      } catch (error) {
-        socket.emit("error-message", { message: "Gagal memulai game" });
-      }
+      const song = await getRandomSong();
+
+      await client.HSET(`rooms:${room_id}:current_song`, song);
+      await client.HSET(`rooms:${room_id}:details`, "status", "playing");
+
+      io.to(room_id).emit("room-update", {
+        status: "playing",
+      });
     });
+
+    socket.on(
+      "submit-answer",
+      async ({ room_id, player_answer, player_id, player_username }) => {
+        const currentSongTitle = await client.HGET(
+          `rooms:${room_id}:current_song`,
+          "title",
+        );
+
+        if (player_answer === currentSongTitle) {
+          await client.ZINCRBY(
+            `rooms:${room_id}:scores`,
+            100,
+            `${player_id}:${player_username}`,
+          );
+        }
+
+        const players = await client.ZRANGE_WITHSCORES(
+          `rooms:${room_id}:scores`,
+          0,
+          -1,
+        );
+
+        const formattedPlayers = players.map((player) => {
+          const [id, username] = player.value.split(":");
+          return {
+            id,
+            username,
+            score: player.score,
+          };
+        });
+
+        io.to(room_id).emit("leaderboard-update", {
+          players: formattedPlayers,
+        });
+      },
+    );
   });
 };
