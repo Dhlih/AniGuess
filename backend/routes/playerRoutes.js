1381ca8e8a2272949d3769291caa85561f07c11a
ifad <yusuffadhlih123@gmail.com>
2026-02-05 21:53:31 +0700

    added player routes

---
+++ backend/routes/playerRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const roomController = require("../controllers/playerController");
+
+router.get("/:playerId/current-room", roomController.getCurrentRoom);
+
+module.exports = router;
