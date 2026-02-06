188bf07fc913c85a3bd0364779104252aaf60e14
unknown <yusuffadhlih123@gmail.com>
2026-02-06 22:29:06 +0700

    deleted player routes

--- backend/routes/playerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const roomController = require("../controllers/playerController");
-
-router.get("/:playerId/current-room", roomController.getCurrentRoom);
-
-module.exports = router;
