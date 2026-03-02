5a94f9557b81bcbd1e9f329825c39f351667b557
ifad <yusuffadhlih123@gmail.com>
2026-03-03 05:23:30 +0700

    Added single player route

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -4,7 +4,9 @@ const roomController = require("../controllers/roomController");
 
 router.get("/", roomController.getRooms);
 
-router.post("/", roomController.createRoom);
+router.post("/multiplayer", roomController.createMultiPlayerRoom);
+
+router.post("/singleplayer", roomController.createSinglePlayerRoom);
 
 router.get("/:roomId/validate", roomController.getRoomAccess);
 
